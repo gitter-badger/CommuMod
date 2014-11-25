c0da6a41a3e9eb47419d228d3b024c0c1ca97180
Noah <n.kovacs1@icloud.com>
2014-11-24 21:38:44 -0500

    Cleaned up gh-pages branch
    Signed-off-by: Noah <n.kovacs1@icloud.com>

--- src/main/java/common/endergriefer/jamoa/help/Reference.java
+++
@@ -1,9 +0,0 @@
-package common.endergriefer.jamoa.help;
-
-
-public class Reference {
-
-    public static final String MODID="endergriefer_jamoa";
-    public static final String VERSION="1.1.1b";
-
-}
